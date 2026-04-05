0e26985fd38a36610884620bd3365e578d1fecdc
TokyoJJ <tokyojj33@gmail.com>
2026-04-05 22:03:17 +0900

    Remove image_processing from repository

--- image_processing/src/check_images.py
+++
@@ -1,9 +0,0 @@
-import re
-
-with open('c:/Users/jhsim/Erica261/머신러닝/presentation_visualizer.html', 'r', encoding='utf-8') as f:
-    text = f.read()
-
-m = re.findall(r'<img [^>]*>', text)
-print(f'Found {len(m)} img tags in presentation_visualizer')
-for match in m:
-    print(match[:100])
