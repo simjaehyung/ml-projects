af95b309ce2ad51b554102f8ad3459675fa3ab22
TokyoJJ <tokyojj33@gmail.com>
2026-04-05 22:05:28 +0900

    Remove web_ui_dashboard from repository

--- web_ui_dashboard/app/backend/__init__.py
+++
@@ -1,7 +0,0 @@
-﻿"""
-app.backend package
-"""
-from .validator import validate_and_format, validate_batch, BoundingBox
-from .storage_module import save_all_data
-from .augmentation_recipe import OnTheFlyAugmenter, sample_augmentation_params
-
